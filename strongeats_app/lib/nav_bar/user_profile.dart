fc721478144f2bbef41965b2bced297861e8b783
MikeLup <91550055+MikeLup@users.noreply.github.com>
2023-11-13 20:36:06 -0500

    Update user_profile.dart

--- strongeats_app/lib/nav_bar/user_profile.dart
+++ strongeats_app/lib/nav_bar/user_profile.dart
@@ -1,21 +1,131 @@
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
+ @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    backgroundColor: Colors.black,
+      body: Center(
+        child: Column( // Use a Column to stack widgets vertically
+          mainAxisAlignment: MainAxisAlignment.center,
+        children:[ ElevatedButton.icon(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()));
+          } ,
+          icon: Icon(
+            Icons.add
+          ),
+          label: Text("User Info"),
+          //color: Colors.black,
+          ),
+          SizedBox(height: 20), // Adding some space between buttons
+          ElevatedButton.icon(
+              onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ThirdRoute()));
+          } ,
+              icon: Icon(Icons.remove),
+              label: Text("Update Goal Weight"),
+            ),
+        
+        SizedBox(height: 20), // Adding some space between buttons
+          ElevatedButton.icon(
+              onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ThirdRoute()));
+          } ,
+              icon: Icon(Icons.history),
+              label: Text("Workout Preferences"),
+            ),
+            
+            SizedBox(height: 20), // Adding some space between buttons
+          ElevatedButton.icon(
+              onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const FourthRoute()));
+          } ,
+              icon: Icon(Icons.history),
+              label: Text("Account Settings"),
+            ),
+        ],
+        ),
+      
+    ),
+    );
+  
+  }
+} 
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                'Profile Page',
-                style: TextStyle(color: Colors.grey[600]),
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Info"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
         ),
       ),
     );
   }
 }
+
+class ThirdRoute extends StatelessWidget {
+  const ThirdRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Update Goal Weight "),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+  class FourthRoute extends StatelessWidget {
+  const FourthRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("User Info"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+
